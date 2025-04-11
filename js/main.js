4e2e65d92cfd7e17940af5edbfe46f969a70c749
mcdrn <reeen11073733@icloud.com>
2025-04-11 20:05:53 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -39,3 +39,23 @@ document.addEventListener("DOMContentLoaded", function () {
   
     // aタグのクリック時にはアコーディオン開閉しないように設定しない（遷移を許可）
   });
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleBtns = document.querySelectorAll(".toggle-btn-footer");
+  
+    toggleBtns.forEach(btn => {
+      btn.addEventListener("click", (event) => {
+        event.stopPropagation(); // ボタンだけが反応するようにする（aタグに影響しない）
+        const content = btn.parentElement.nextElementSibling;
+        content.classList.toggle("open");
+        btn.classList.toggle("rotate");
+  
+        if (content.classList.contains("open")) {
+          content.style.maxHeight = content.scrollHeight + "px";
+        } else {
+          content.style.maxHeight = null;
+        }
+      });
+    });
+  
+    // aタグのクリック時にはアコーディオン開閉しないように設定しない（遷移を許可）
+  });
